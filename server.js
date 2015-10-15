1462deadc2054f219fcdb5ff8e8465c495c53471
Magno Dias <ciqueira@hotmail.com>
2015-10-15 16:48:15 -0300

    fix8aaab1

--- server.js
+++ server.js
@@ -25,27 +25,6 @@ db.on('error', function () {
 
 var port = process.env.PORT || 1337;
 
-
-var oauth = require('./controllers/oauth'),
-    music = require('./controllers/music'),
-    drive = require('./controllers/drive'),
-    user = require('./controllers/user'),
-    auth = require('./middlewares/authResource');
-    
-    app.post('/api/oauth/token', oauth.token);
-    
-    app.all('/api/*', auth); 
-
-    app.post('/api/drive', drive.sync);
-    app.get('/api/music/artist', music.allArtist);
-    app.post('/api/music/start', music.createSession);
-
-    app.get('/api/user/:user_type', user.findUserByType);
-    app.post('/api/user/:user_id/Follow', user.follow);
-    app.delete('/api/user/:user_id/Follow', user.unfollow);
-    app.get('/api/user/:keyword/searchByName', user.searchByName);
-
-
 //require(routes)(app);
 
 app.listen(port);
