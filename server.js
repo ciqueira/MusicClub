9ae2fb03784a7553d5568cf052070f9fb767f6df
Magno Dias <ciqueira@hotmail.com>
2015-10-15 17:00:24 -0300

    fix9aaaaa

--- server.js
+++ server.js
@@ -27,25 +27,5 @@ var port = process.env.PORT || 1337;
 
 //require('./routes')(app);
 
-
-var oauth = require('./controllers/oauth'),
-        music = require('./controllers/music'),
-        drive = require('./controllers/drive'),
-        user = require('./controllers/user'),
-        auth = require('./middlewares/authResource');
-    
-    app.post('/api/oauth/token', oauth.token);
-    
-    app.all('/api/*', auth); 
-
-    app.post('/api/drive', drive.sync);
-    app.get('/api/music/artist', music.allArtist);
-    app.post('/api/music/start', music.createSession);
-
-    app.get('/api/user/:user_type', user.findUserByType);
-    app.post('/api/user/:user_id/Follow', user.follow);
-    app.delete('/api/user/:user_id/Follow', user.unfollow);
-    app.get('/api/user/:keyword/searchByName', user.searchByName);
-
 app.listen(port);
 console.log('Listening... ');
