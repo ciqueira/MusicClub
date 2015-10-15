7a50049782a2830bd58f633bfda314d20317e131
Magno Dias <ciqueira@hotmail.com>
2015-10-15 16:37:24 -0300

    fix8abc

--- server.js
+++ server.js
@@ -36,18 +36,5 @@ var port = process.env.PORT || 1337;
 
 //require(routes)(app);
 
-    app.post('/api/oauth/token', oauth.token);
-    
-    app.all('/api/*', auth); 
-
-    app.post('/api/drive', drive.sync);
-    app.get('/api/music/artist', music.allArtist);
-    app.post('/api/music/start', music.createSession);
-
-    app.get('/api/user/:user_type', user.findUserByType);
-    app.post('/api/user/:user_id/Follow', user.follow);
-    app.delete('/api/user/:user_id/Follow', user.unfollow);
-    app.get('/api/user/:keyword/searchByName', user.searchByName);
-
 app.listen(port);
 console.log('Listening... ');
