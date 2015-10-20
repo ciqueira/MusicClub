381b5b48bc820dbd3446c5691a7dfea32880936e
Magno Dias <ciqueira@hotmail.com>
2015-10-19 23:58:14 -0200

    fix16a

--- controllers/music.js
+++ controllers/music.js
@@ -1,5 +1,5 @@
 ﻿var mongoose = require('mongoose'),
-    Music = require('../models/music');// mongoose.model('Music');
+    Music = require('./../models/music');// mongoose.model('Music');
 //     userController = require('./user.js');
 // var jwt = require('jsonwebtoken');
 
